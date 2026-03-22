5b912d938dc15f057ff449396fb280ae43c50d07
Wamiq Abbasi <wamiqabbasi@ee:17:5c:33:a4:c9.home>
2026-03-22 16:02:29 -0500

    Enable demo login in prod and prioritize auth errors

--- backend/src/main/java/com/brightpath/lms/auth/AuthExceptionHandler.java
+++ backend/src/main/java/com/brightpath/lms/auth/AuthExceptionHandler.java
@@ -6,12 +6,15 @@ import com.brightpath.lms.auth.exception.TooManyLoginAttemptsException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class AuthExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(AuthExceptionHandler.class);
